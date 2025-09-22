12a53413391572f52410406004a536919f25fabe
asukas00 <jessere2025@gmail.com>
2025-09-22 07:17:08 -0700

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,11 @@
 import random
 
-random.randint(1,100)
+answer = random.randint(1,10)
+guess = (input("guess a number between 1-10"))
+
+if guess >= answer:
+    print("your number is too high")
+elif guess == answer:
+    print("you got it")
+elif guess <= answer:
+    print("your number is too low")
