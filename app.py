657b3d81fc24232509fe1e286ea3a5ab2134ebb8
asukas00 <jessere2025@gmail.com>
2025-09-26 07:17:35 -0700

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import random
 
-answer = random.randint(1,100)
-print("guess a number between 1-100")
+answer = random.randint(1,10000)
+print("guess a number between 1-10000")
 attempt = 0
 guess_history = []
 while True:
@@ -11,10 +11,12 @@ while True:
             print("Too low. Try again.")
             attempt += 1
             guess_history.append(guess)
+            print(guess_history)
         elif guess > answer:
             print("Too high. Try again.")
             attempt += 1
             guess_history.append(guess)
+            print(guess_history)
         else:
             attempt += 1
             print(f"YAY! you got it in {attempt} attempts!")
